60214e48d04d056441c76ffae96515a756eadf72
n-kolin <1020nomi@gmail.com>
2025-06-03 13:50:01 +0300

    lll

--- react-app/src/components/Menu.tsx
+++ react-app/src/components/Menu.tsx
@@ -24,7 +24,7 @@ const Menu = () => {
           <div className="logo-container">
             <div className="logo-wrapper">
               <div className="logo-icon">
-                <img src="/logo2.gif?height=32&width=32" alt="Logo" className="logo-img" />
+                <img src="/lll.gif?height=32&width=32" alt="Logo" className="logo-img" />
                 <div className="logo-inner"></div>
               </div>
               <h1 className="logo-text">
